170915cdfe959ecda7af26249a106624f7f5c624
A. Martin Castro <castroarielm@gmail.com>
2018-03-13 22:22:06 -0700

    configure firebase database libraries

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyA9LDU0L-Y1xurgBNyW2XGwj8T8cs9_iJw",
+    authDomain: "shopping-list-ef330.firebaseapp.com",
+    databaseURL: "https://shopping-list-ef330.firebaseio.com",
+    projectId: "shopping-list-ef330",
+    storageBucket: "shopping-list-ef330.appspot.com",
+    messagingSenderId: "12276661984"
+  };
